2e78d0eb182db775445d8dd9b0eaf47ab1312239
CapAgenda <capitalistagenda@pm.me>
2022-09-04 23:55:04 -0700

    project sections

---
+++ src/components/ProjectSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'work',
+    lightBg: false,
+    lightText: true,
+    LightTextDesc: true,
+    topLine: 'Python / React / GitHub Actions / Requests',
+    headline: 'Comic Content Management System',
+    description: 'A React web application that pulls images from an RSS feed; and dynamically renders them in a webpage',
+    buttonLabel: 'View Details',
+    imgStart: false,
+    img: require('../../images/csb-img1.svg').default,
+    alt: 'Comic Strip CMS',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'work2',
+    lightBg: true,
+    lightText: false,
+    LightTextDesc: false,
+    topLine: 'Python / React / GitHub Actions / Finance',
+    headline: 'Trading and economic data notebook',
+    description: 'Macro data web application that visualizes and organizes a money management process to allow traders to understand what is going on in the market and make better decisions',
+    buttonLabel: 'View Details',
+    imgStart: true,
+    img: require('../../images/macro-img1.svg').default,
+    alt: 'Macro Trading',
+    dark: false,
+    primary: false,
+    darkText: true,
+};
+
+export const homeObjThree = {
+    id: 'work3',
+    lightBg: false,
+    lightText: true,
+    LightTextDesc: true,
+    topLine: 'Illustration / Design / Podcasting ',
+    headline: 'Custom artwork and illustration',
+    description: 'Selection of artwork and designs created including podcast album artwork, product labels and custom logos',
+    buttonLabel: 'View Details',
+    imgStart: false,
+    img: require('../../images/art-img1.svg').default,
+    alt: 'Comic Strip CMS',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import HeroSection from '../components/HeroSection'
 import Navbar from '../components/Navbar'
+import ProjectSection from '../components/ProjectSection'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/ProjectSection/Data'
 import Sidebar from '../components/Sidebar'
 
 const Home = () => {
@@ -15,6 +17,9 @@ const Home = () => {
     <Sidebar isOpen={isOpen} toggle={toggle}/>
     <Navbar isOpen={isOpen} toggle={toggle} />
     <HeroSection />
+    <ProjectSection {...homeObjOne}/>
+    <ProjectSection {...homeObjTwo}/>
+    <ProjectSection {...homeObjThree}/>
     </>
   )
 }
